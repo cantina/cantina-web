70daccba94a53172b8883eeb200db28e4f9f8a66
Brian Link <cpsubrian@gmail.com>
2013-07-25 19:14:49 -0700

    Adds simple static file test.

--- test/basic.js
+++ test/basic.js
@@ -1,3 +1,27 @@
 describe('basic test', function () {
-  it('works');
+  var proc;
+
+  before(function (done) {
+    proc = spawn('node', ['example.js'], {cwd: example});
+    process.on('exit', function () {
+      proc.kill();
+    });
+    proc.stdout.on('data', function (data) {
+      assert.equal(data.toString(), 'Listening on 0.0.0.0:3000\n');
+      done();
+    });
+  });
+
+  after(function () {
+    if (proc) proc.kill();
+  });
+
+  it('GET /css/style.css', function (done) {
+    request('http://localhost:3000/css/style.css', function (err, res, body) {
+      assert.ifError(err);
+      assert.equal(res.statusCode, 200);
+      assert(res.headers['content-type'].match(/text\/css/));
+      done();
+    });
+  });
 });
--- test/common.js
+++ test/common.js
@@ -1,2 +1,6 @@
 assert = require('assert');
 util = require('util');
+spawn = require('child_process').spawn;
+exec = require('child_process').exec;
+example = require('path').resolve(__dirname, '../example');
+request = require('request');
