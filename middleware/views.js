199e293f4820672794846de4cda6b2175e9cc285
erinlesavoy <erin.lesavoy@gmail.com>
2014-04-09 14:49:28 -0400

    Moves helpers to views middleware instead of templ-multi

--- middleware/views.js
+++ middleware/views.js
@@ -5,7 +5,28 @@ require('../plugins/views');
 
 // Export middleware.
 if (app.viewsHandler) {
-  module.exports = app.viewsHandler;
+  module.exports = function (req, res, next) {
+    app.viewsHandler(req, res, function () {
+
+      // Run a hook before render so plugins can intercept
+      var _render = res.render;
+      res.render = function (template, context, options) {
+        app.hook('views:before:render:' + template).runSeries(req, res, context, options, function (err) {
+          if (err) return app.emit('error', err);
+          _render(template, context, options);
+        });
+      };
+
+      // Emit an error and respond to user with a 500.
+      res.renderError = function (err) {
+        app.emit('error', err.stack || err);
+        res.statusCode = 500;
+        res.renderStatus(500, res.vars);
+      };
+
+      next();
+    });
+  };
   module.exports.weight = -900;
 }
 else {
