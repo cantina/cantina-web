680715d4858778f29f03dc0890c5c9bd5430ebe4
erinlesavoy <erin.lesavoy@gmail.com>
2014-04-09 15:17:11 -0400

    Defaults handlebars helper context to this

--- plugins/handlebars.js
+++ plugins/handlebars.js
@@ -2,11 +2,16 @@ var app = require('cantina');
 
 require('./views');
 
-app.Handlebars.registerHelper('applyPartial', function (templateName, vars) {
+app.Handlebars.registerHelper('applyPartial', function (templateName, vars, options) {
+  if (arguments.length < 3) {
+    options = vars;
+    vars = null;
+  }
   var template = app.Handlebars.partials[templateName];
   if (!template) {
     throw new Error('Handlebars partial ' + templateName + ' could not be found.');
   }
-  return template(vars);
+  return template(vars || this);
 });
+
 
